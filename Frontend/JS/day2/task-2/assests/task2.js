b95c24ffc33ce8a4a3095b4304f29d1aa57c9902
Pavithran <vetriselvan0608@gmail.com>
2026-09-22 23:47:04 +0530

    updated

---
+++ Frontend/JS/day2/task-2/assests/task2.js
@@ -0,0 +1,10 @@
+var abc;
+var nameId
+let studentMark
+let studentAge
+var totalMark
+console.log(abc);
+console.log(nameId);
+console.log(studentMark);
+console.log(studentAge);
+console.log(totalMark);
---
+++ Frontend/JS/day2/task-3/script.js
@@ -0,0 +1,103 @@
+//var with string values
+var nameone="Vetri"
+var nametwo="Java"
+
+
+console.log(nameone);
+console.log(nametwo);
+
+
+//var with number
+var numOne=5
+var numTwo=6
+
+
+console.log(numOne);
+console.log(numTwo);
+
+
+
+//var with undefined
+var age
+var long
+
+
+
+console.log(age);
+console.log(long);
+
+
+//var with boolean
+var isLoggedIn=true
+var isLoggedOut=false
+
+console.log(isLoggedIn);
+console.log(isLoggedOut);
+
+//var with null
+var teamOne=null
+var teamTwo=null
+
+console.log(teamOne)
+console.log(teamTwo)
+
+//let with string
+let nameThree="Python"
+let nameFour="CSS"
+
+
+console.log(nameThree);
+console.log(nameFour);
+
+
+//let with number
+let numThree=7
+let numFour=8
+
+console.log(numThree);
+console.log(numFour);
+
+//let with undefined
+let marks
+let score
+
+console.log(marks);
+console.log(score);
+
+//let with boolean
+let isTimeOut=true
+let isRetained=false
+
+
+console.log(isTimeOut);
+console.log(isRetained);
+
+
+//let with null
+let countryThree=null
+let countryFour=null
+
+console.log(countryThree)
+console.log(countryFour)
+
+//const with string
+
+const nameFive="C##"
+console.log(nameFive);
+
+//const with number
+const numFive=9
+
+console.log(numFive);
+
+//const with undefined
+const vote=18
+console.log(vote)
+
+//const with boolean
+const isSelected=true
+console.log(isSelected);
+
+//const with  null
+const countryFive=null
+console.log(countryFive)
